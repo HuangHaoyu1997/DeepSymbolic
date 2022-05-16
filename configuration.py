616d6c7b9397e199278ecd6e4bfff55dea4bc9bb
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-05-16 14:46:59 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,11 +1,11 @@
 class config:
     seed = 123
     lr = 1e-3
-    rollout_episode = 10
-    num_steps = 1000
+    rollout_episode = 5
+    num_steps = 200
     num_episodes = 100000
     ckpt_freq = 10
-    num_parallel = 80
+    num_parallel = 40
     zero_coef = 1.0 # 鼓励采用尽可能多的None操作
     # for CMA-ES
     sigma_init = 3.5
