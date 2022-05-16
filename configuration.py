5a06823a5b65729211a3cf37ff9246f34cb45b13
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-05-16 15:26:52 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -4,7 +4,7 @@ class config:
     rollout_episode = 5
     num_steps = 200
     num_episodes = 100000
-    ckpt_freq = 10
+    ckpt_freq = 2
     num_parallel = 40
     zero_coef = 1.0 # 鼓励采用尽可能多的None操作
     # for CMA-ES
