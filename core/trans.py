30db242bfbb9746b2d5c87a1ed9bb7ff22694fd2
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-05-22 12:21:42 +0800

    1

--- core/trans.py
+++ core/trans.py
@@ -8,7 +8,7 @@ d_embed = 7 # embedding dimension
 n_heads = 8
 d_k = 16
 d_hidden = 16
-d_classification = 2
+d_class = 2
 n_layers = 4 # Encoder内含
 trajectory = torch.rand(batch_size, len_traj, d_obs)
 
@@ -23,96 +23,103 @@ class Embedding(nn.Module):
         return x
 
 class ScaledDotProductAttention(nn.Module):
-    def __init__(self):
+    def __init__(self, d_k):
         super(ScaledDotProductAttention, self).__init__()
+        self.d_k = d_k
     
     def forward(self, Q, K, V):
         # scores : [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)] [1,8,5,5]
-        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k)
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(self.d_k)
         attn = nn.Softmax(dim=-1)(scores)
         context = torch.matmul(attn, V)
         return context, attn
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self):
+    def __init__(self, d_embed, d_k, n_heads):
         super(MultiHeadAttention, self).__init__()
+        self.n_heads = n_heads
+        self.d_k = d_k
         self.W_Q = nn.Linear(d_embed, d_k * n_heads) # d_embed,7维, d_k,16*8=128维
         self.W_K = nn.Linear(d_embed, d_k * n_heads)
         self.W_V = nn.Linear(d_embed, d_k * n_heads)
         self.fc = nn.Linear(n_heads * d_k, d_embed)
         self.layer_norm = nn.LayerNorm(d_embed)
+        self.DotProduct = ScaledDotProductAttention(d_k)
 
     def forward(self, x):
         # q: [batch_size x len_q x d_model], k: [batch_size x len_k x d_model], v: [batch_size x len_k x d_model]
         residual, batch_size = x, x.size(0) # 残差跨层连接
         
-        q_s = self.W_Q(x).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # q_s: [batch_size x n_heads x len_q x d_k]
-        k_s = self.W_K(x).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # k_s: [batch_size x n_heads x len_q x d_k]
-        v_s = self.W_V(x).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # v_s: [batch_size x n_heads x len_q x d_k]
+        # q_s = k_s = v_s: [batch_size, n_heads, len_q, d_k]
+        q_s = self.W_Q(x).view(batch_size, -1, self.n_heads, self.d_k).transpose(1,2)
+        k_s = self.W_K(x).view(batch_size, -1, self.n_heads, self.d_k).transpose(1,2)
+        v_s = self.W_V(x).view(batch_size, -1, self.n_heads, self.d_k).transpose(1,2)
         
-        # context: [batch_size x n_heads x len_q x d_k]
-        # attn: [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
-        context, attn = ScaledDotProductAttention()(q_s, k_s, v_s) # context是attn✖V
+        # context: [batch_size, n_heads, len_q, d_k]
+        # attn: [batch_size, n_heads, len_q(=len_k), len_k(=len_q)]
+        context, attn = self.DotProduct(q_s, k_s, v_s) # context是attn✖V
         # contiguous()的功能类似deepcopy
-        context = context.transpose(1, 2).contiguous().view(batch_size, -1, n_heads * d_k) # context: [batch_size x len_q x n_heads * d_k] 最后一个维度是将8个head concat起来，维度依然512
+        context = context.transpose(1, 2).contiguous().view(batch_size, -1, self.n_heads * self.d_k) # context: [batch_size x len_q x n_heads * d_k] 最后一个维度是将8个head concat起来，维度依然512
         
         output = self.fc(context) # [batch_size, len_q, d_embed]
-        return self.layer_norm(output + residual), attn # output: [batch_size x len_q x d_model]
+        return self.layer_norm(output + residual), attn # output: [batch_size, len_q, d_model]
 
 class PoswiseFeedForwardNet(nn.Module):
     # 该模块也可用linear+ReLU实现
-    def __init__(self):
+    def __init__(self, d_embed, d_hidden):
         super(PoswiseFeedForwardNet, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=d_embed, out_channels=d_hidden, kernel_size=1)
         self.conv2 = nn.Conv1d(in_channels=d_hidden, out_channels=d_embed, kernel_size=1)
         self.layer_norm = nn.LayerNorm(d_embed)
     def forward(self, x):
-        residual = x # inputs : [batch_size, len_q, d_model]
+        residual = x # [batch_size, len_q, d_model]
         x = nn.ReLU()(self.conv1(x.transpose(1, 2)))
         x = self.conv2(x).transpose(1, 2)
         return self.layer_norm(x + residual)
 
 class EncoderLayer(nn.Module):
-    def __init__(self):
+    def __init__(self, d_embed, d_k, n_heads, d_hidden):
         super(EncoderLayer, self).__init__()
-        # 1个encoder layer由2个模块组成
-        self.MultiHeadAttention = MultiHeadAttention()
-        self.PoswiseFeedForwardNet = PoswiseFeedForwardNet()
+        self.MultiHeadAttention = MultiHeadAttention(d_embed, d_k, n_heads)
+        self.PoswiseFeedForwardNet = PoswiseFeedForwardNet(d_embed, d_hidden)
 
     def forward(self, x):
-        x, attn = self.MultiHeadAttention(x) # enc_inputs to same Q,K,V
-        x = self.PoswiseFeedForwardNet(x) # enc_outputs: [batch_size x len_q x d_model]
+        x, attn = self.MultiHeadAttention(x) # x to same Q,K,V
+        x = self.PoswiseFeedForwardNet(x) # x: [batch_size, len_q, d_embed]
         return x, attn
 
 class Encoder(nn.Module):
-    def __init__(self):
+    '''
+    using transformer encoder to classify sequential data
+    '''
+    def __init__(self, d_obs, d_embed, d_class, d_k, d_hidden, n_heads, n_layers):
         super(Encoder, self).__init__()
         self.embedding = Embedding(inpt_dim=d_obs, embed_dim=d_embed) # state dimension，embedding dimension
-        self.layers = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])
-        self.fc = nn.Linear(d_embed, d_classification)
+        self.layers = nn.ModuleList([EncoderLayer(d_embed, d_k, n_heads, d_hidden) for _ in range(n_layers)])
+        self.fc = nn.Linear(d_embed, d_class)
 
     def forward(self, x): # enc_inputs : [batch_size x source_len]
         y = self.embedding(x)
-        self_attns = []
+        attentions = []
         for layer in self.layers:
-            y, self_attn = layer(y)
-            self_attns.append(self_attn)
+            y, attention = layer(y)
+            attentions.append(attention)
         
         y = y.mean(dim=1) # [batch_size, d_embed]
         out = torch.softmax(self.fc(y), dim=-1)
-        return out, self_attns
+        return out, attentions
+
+if __name__ == '__main__':
+    encoder = Encoder(d_obs, d_embed, d_class, d_k, d_hidden, n_heads, n_layers)
+    for _ in range(10):
+        trajectory = torch.rand(batch_size, len_traj, d_obs)
+        pred, _ = encoder(trajectory)
+        print(torch.argmax(pred,-1))
 
 
-encoder = Encoder()
-for _ in range(10):
-    trajectory = torch.rand(batch_size, len_traj, d_obs)
-    pred, _ = encoder(trajectory)
-    print(torch.argmax(pred,-1))
+    '''
+    from torchinfo import summary
+    summary(encoder, (batch_size, len_traj, d_obs))
+    print(context.shape, attn[0].shape)
 
-
-'''
-from torchinfo import summary
-summary(encoder, (batch_size, len_traj, d_obs))
-print(context.shape, attn[0].shape)
-
-'''
+    '''
--- test.py
+++ test.py
@@ -1,1 +1,50 @@
-import paves
+import os, pickle
+import numpy as np
+from core.trans import Encoder
+
+batch_size = 16
+d_obs = 13
+d_embed = 16 # embedding dimension
+n_heads = 8
+d_k = 32
+d_hidden = 32
+d_class = 2
+n_layers = 4
+encoder = Encoder(d_obs, d_embed, d_class, d_k, d_hidden, n_heads, n_layers)
+
+def data_generate(dir = './data/', action_dim = 4):
+    with open(dir+'good_trajectories.pkl', 'rb') as f:
+        trajectories = pickle.load(f)
+
+    batch_data = []
+    for i, traj in enumerate(trajectories):
+        new_traj = []
+        for transition in traj:
+            one_hot = np.zeros((action_dim), dtype=np.float32)
+            action = transition[1]
+            one_hot[action] = 1
+            tran = np.concatenate((transition[0], one_hot, [transition[2]]))
+            new_traj.append(tran)
+        batch_data.append(new_traj)
+        if len(batch_data)==1000:
+            with open('./data/batch_'+str(i+1)+'.pkl', 'wb') as f:
+                pickle.dump(batch_data, f)
+                batch_data = []
+    with open('./data/batch_data/batch_'+str(i+1)+'.pkl', 'wb') as f:
+        pickle.dump(batch_data, f)
+
+def get_batch_data(dir='./data/batch_data/'):
+    total_data = []
+    dlist = os.listdir(dir)
+    for d in dlist:
+        with open(dir+d, 'rb') as f:
+            total_data.extend(pickle.load(f))
+    return total_data
+data = get_batch_data()
+num_samples = len(data)
+iterations = num_samples // batch_size
+fake_data = np.random.rand(5000, 500, 13)
+
+for i in range(iterations):
+    
+
