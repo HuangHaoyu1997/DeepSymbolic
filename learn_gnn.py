45462350161eb0e00d29f3cbb5060c3814502d8c
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-06-12 15:13:18 +0800

    1

--- learn_gnn.py
+++ learn_gnn.py
@@ -5,3 +5,42 @@ from torch_geometric.datasets import Planetoid
 
 dataset = Planetoid(root='./data/Cora', name='Cora')
 
+print(dataset.num_node_features)
+print(dataset.num_classes)
+print(dataset.num_edge_features)
+print(dataset.data)
+
+class GCN_Net(torch.nn.Module):
+    def __init__(self, features, hidden, classes):
+        super(GCN_Net, self).__init__()
+        self.conv1 = GCNConv(features, hidden)
+        self.conv2 = GCNConv(hidden, classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index) 
+        print(x.shape)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = GCN_Net(dataset.num_node_features, 16, dataset.num_classes).to(device)
+data = dataset[0].to(device)	
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+model.train()
+for epoch in range(500):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+    print(epoch, loss.item())
+
+model.eval()
+_, pred = model(data).max(dim=1)
+correct = pred[data.test_mask].eq(data.y[data.test_mask]).sum()
+acc = int(correct) / int(data.test_mask.sum())
+print('GCN:', acc)
