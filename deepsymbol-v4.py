69515e35db036cc00cc3d36624d4c6aca7867017
Haoyu Huang <446700679@qq.com>
2022-06-13 01:16:36 +0800

    Update deepsymbol-v4.py

--- deepsymbol-v4.py
+++ deepsymbol-v4.py
@@ -12,10 +12,25 @@ class Individual:
         self.N = N
         self.L = (N+M)*N
         self.threshold = 0.5
+        self.genetype = self.gene()
 
     def gene(self,):
         '''generate gene'''
         tmp = np.zeros((self.L),dtype=np.int16)
         idx = np.array([random.random() for _ in range(self.L)])
-        np.where(idx>self.threshold)
-        return
+        idx = np.where(idx>self.threshold)
+        tmp[idx] = 1
+        return tmp
+
+def population(Npop, M, N):
+    '''generate population with size Npop'''
+    return [Individual(M,N) for _ in range(Npop)]
+
+def translate(ind:Individual):
+    '''map the individual genetype to a graph'''
+    gene = ind.genetype
+    N, M = ind.N, ind.M
+    adj_dict = {}
+    for i in range(N):
+        gene_batch = gene[i*(N+M):(i+1)*(N+M)]
+        
